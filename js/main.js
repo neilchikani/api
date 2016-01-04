f68f1c60974753931d0c6a0e6a9603173d555fb5
Nilay Chikani <nilay.chikani@raweng.com>
2016-01-04 17:59:15 +0530

    adding files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+ 
 
-var myApp = angular.module("myApp", ['ngRoute']);
 
+var myApp = angular.module("myApp", ['textAngular','ngRoute']);
+myApp.directive('file', function () {
+    return {
+        scope: {
+            file: '='
+        },
+        link: function (scope, el, attrs) {
+            el.bind('change', function (event) {
+                console.log(event);
+                var file = event.target.files[0];
+                scope.file = file ? file : undefined;
+                scope.$apply();
+            });
+        }
+    };
+});
 myApp.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
@@ -17,110 +33,56 @@ myApp.config(['$routeProvider',
 
             });
     }]);
-//myApp.controller('userCtrl', function ($scope ,$http) {
-//    $scope.fName = '';
-//    $scope.lName = '';
-//    $scope.edit = false;
-//    $scope.create = false;
-//    $scope.template = false;
-//    $scope.button = true;
-//
-//
-//    $http.get("http://172.16.0.180:3000") .success(function(response,data) {
-//        $scope.users = response;
-//    });
-//
-//    $scope.submit = function (id) {
-//
-//        if (id == 'new') {
-//            $scope._actionType = 'new';
-//            $scope.button = true;
-//            $scope.create = true;
-//            $scope.template = true;
-//            $scope.edit = false;
-//        }else{
-//            $scope._actionType = id;
-//            $scope.button = false;
-//            $scope.create = false;
-//            $scope.template = true;
-//            $scope.edit = true;
-//            $scope.users.forEach(function(index){
-//                if(index.id == id){
-//                    $scope.fName = index.Fname;
-//                    $scope.lName = index.Lname;
-//                }
-//            });
-//        }
-//    };
-//
-//    $scope.save = function(action){
-//
-//        if(action == 'new'){
-//            if ($scope.fName && $scope.lName) {
-//                var data = {
-//                    fname : $scope.fName,
-//                    lname : $scope.lName
-//                };
-//                $http.post("http://172.16.0.180:3000",data)
-//                    .success(function(response) {
-//                    });
-//                $scope.fName = '';
-//                $scope.lName = '';
-//                $scope.passw1 = '';
-//                $scope.passw2 = '';
-//            }else{
-//                alert('please enter fname and lname');
-//            }
-//        }else{
-//
-//            if ($scope.fName && $scope.lName) {
-//                var data = {
-//                    fname : $scope.fName,
-//                    lname : $scope.lName
-//                };
-//                $http.put("http://172.16.0.180:3000/edit/"+action,data)
-//                    .success(function(response) {
-//                    });
-//                $scope.fName = '';
-//                $scope.lName = '';
-//                $scope.passw1 = '';
-//                $scope.passw2 = '';
-//            }else{
-//                alert('please enter fname and lname');
-//            }
-//        }
-//    };
-//
-//    $scope.deleteUser = function (ids) {
-//        $http.delete("http://172.16.0.180:3000/delete/"+ids)
-//            .success(function(response) {
-//
-//            });
-//    };
-//});
-myApp.controller('admin', function($scope, $http) {
+myApp.controller('admin', function admin($scope, $http) {
+     $scope.htmlContent = 'Hello';
 
     $scope.submit = function(){
+        
+        
         if($scope.email && $scope.password){
-            var data = {
-                email : $scope.email,
-                password : $scope.password
-            };
-            $http.post("http://localhost:3000/insert",data).success(function(response) {
-                console.log(response);
+             
+             $http({
+                method: 'POST',
+                url: 'http://localhost:3000/insert',
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                data: {
+                    email : $scope.email,
+                    password : $scope.password,
+                    richText : $scope.htmlContent,
+                    image: $scope.file
+                },
+                transformRequest: function (data, headersGetter) {
+                    var formData = new FormData();
+                    angular.forEach(data, function (value, key) {
+                        formData.append(key, value);
+                    });
+                    console.log(formData);
+                    var headers = headersGetter();
+                    delete headers['Content-Type'];
+
+                    return formData;
+                }
+            })
+            .success(function (data) {
+                alert("data has been submitted");
+            })
+            .error(function (data, status) {
+                alert("error occured");
             });
+             
+            
         }else{
             alert('please input something');
         }
     };
 
 });
-myApp.controller('userList', function($scope, $http) {
-     
+myApp.controller('userList', function ($scope, $http) {
+    $scope.url = "http://localhost:3000/";
+    
     $http.get("http://localhost:3000/user").success(function(response) {
-        console.log(response);
-        $scope.user = response;
+         $scope.user = response;
     });
-     
-     
 });
